55ba6e21fb36dcd4b1bf8282371a37aa3ee355df
Maria M <muratowa49@gmail.com>
2021-07-06 15:34:15 +0300

    Update StartProgram.java

--- src/main/java/crud/example/json/springbootdemojson/StartProgram.java
+++ src/main/java/crud/example/json/springbootdemojson/StartProgram.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartProgram {
 	public static void main(String[] args) {
-
+		System.out.println("Hello");
 	}
 }
